dea3f56ee9012bc26277e937270db2658fc75fc5
James Taylor <james387@gmail.com>
2020-09-29 18:59:27 -0400

    inherit from Entity class, add more functionality and parameters including the gen_age method from Entity class

--- Agreement.py
+++ Agreement.py
@@ -1,11 +1,20 @@
 """
 Agreement class between persons or institutions.
 """
+from Entity import *
 
-class Agreement(object):
+class Agreement(Entity):
     """docstring for Agreement class"""
 
-    def __init__(self):
-        #super(, self).__init__()
-        self.action = None
-        self.when = None
+    def __init__(self, entered, parties, actions, when, now=dt.datetime.now()):
+        super().__init__(entered)
+        self.age = round(self.gen_age(now), 3)
+        self.parties = parties
+        self.actions = actions
+        self.when = when
+        self.existent = True
+
+    def terminate(self, end_date=dt.datetime.now()):
+        self.end_date = end_date
+        self.age = round(self.gen_age(end_date), 3)
+        self.existent = False
