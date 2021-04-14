58495ec4ab16fcf17533260c88e79b53d2e7708b
James Taylor <james.taylor@gatech.edu>
2021-04-13 23:00:44 -0400

    modify params and import

--- Agreement.py
+++ Agreement.py
@@ -1,20 +1,18 @@
 """
 Agreement class between persons or institutions.
 """
-from Entity import *
+from Entity import Entity
 
 class Agreement(Entity):
-    """docstring for Agreement class"""
+    """Agreement class"""
 
-    def __init__(self, entered, parties, actions, when, now=dt.datetime.now()):
-        super().__init__(entered)
-        self.age = round(self.gen_age(now), 3)
+    def __init__(self, name, parties, actions, when=dt.datetime.now()):
+        super().__init__(name, when)
         self.parties = parties
         self.actions = actions
-        self.when = when
-        self.existent = True
+        self.current = True
 
     def terminate(self, end_date=dt.datetime.now()):
         self.end_date = end_date
         self.age = round(self.gen_age(end_date), 3)
-        self.existent = False
+        self.current = False
