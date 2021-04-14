e4d2785e78c5fb097d7c25d2a24eaece9cc6757d
James Taylor <james.taylor@gatech.edu>
2021-04-13 23:02:36 -0400

    modify params and import

--- Person.py
+++ Person.py
@@ -1,25 +1,22 @@
 """
 Person class for humans, utilizing Entity class.
 """
-from math import *
+import math
 
-from Entity import *
+from Entity import Entity
 import questions
 
 class Person(Entity):
     """Person as individual human entity."""
 
-    def __init__(self, name, sex, dob, smoker=False):
-        super().__init__(dob)
-        self.name = name
+    def __init__(self, name, dob, sex, smoker=False):
+        super().__init__(name, dob)
         self.sex = sex
-        self.dob = dob
         self.smoker = smoker
-        self.age = round(self.gen_age(), 3)
         self.utility = 1
 
     def get_utility(self, ret):
-        return self.utility * log(ret)
+        return self.utility * math.log(ret)
 
     def gen_utility(self):
         qx_1 = "Would you prefer $1,000 today (a) or a 10% chance of $10,000 today (b)?\n"
