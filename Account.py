c26a1d68ae7b8aa6b9c59c7986ffafa34796a53e
James Taylor <james.taylor@gatech.edu>
2023-03-08 12:49:00 -0500

    include accounts

---
+++ Account.py
@@ -0,0 +1,26 @@
+import datetime as dt
+
+from Agreement import Agreement
+
+
+class Account(Agreement):
+    """Account class
+    input: account holder name
+      institution counterparty name
+      amount in the account
+      when the account was created
+    """
+    def __init__(self, holder, institution, amount, opened=dt.datetime.now()):
+        super().__init__(
+            name=f"{institution} account",
+            parties=[holder, institution],
+            actions=["hold"],
+            when=opened,
+        )
+        self.dollars = amount
+
+    def increase(amount):
+        self.dollars += amount
+
+    def decrease(amount):
+        self.dollars -= amount
